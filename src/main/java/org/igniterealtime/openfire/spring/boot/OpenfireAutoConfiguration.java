aa7d58de9e3e9b24df9fcdea7de3de1b6ca65eae
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 15:55:30 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/igniterealtime/openfire/spring/boot/OpenfireAutoConfiguration.java
+++ src/main/java/org/igniterealtime/openfire/spring/boot/OpenfireAutoConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 /**
  * Auto-configuration for Openfire.
  *
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  * @since 1.0.0
  */
 @Configuration
--- src/main/java/org/igniterealtime/openfire/spring/boot/OpenfireProperties.java
+++ src/main/java/org/igniterealtime/openfire/spring/boot/OpenfireProperties.java
@@ -20,7 +20,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 /**
  * Configuration properties for Openfire.
  *
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  * @since 1.0.0
  */
 @ConfigurationProperties(OpenfireProperties.PREFIX)
